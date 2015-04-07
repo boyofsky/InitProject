3f57a0d9d96f397a0b659daa00f6318f4909422a
YenYu <boyofsky.chen@gmail.com>
2015-04-07 19:35:56 +0800

    add member system

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   
+  devise_for :users
   # rails g controller home index  產生index 頁面
   # You can have the root of your site routed with "root"
   root 'home#index'
